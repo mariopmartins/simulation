09dd9e47933a81f55e57f4503aa5b0226f89457c
Mario Martins <mariopmartins@gmail.com>
2020-11-23 13:27:58 -0800

    Person example

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,30 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(animate_sprite_system)
-        .add_system(bevy::input::system::exit_on_esc_system.system())
+        .add_plugin(HelloPlugin)
         .run();
 }
 
+struct Person;
+struct Name(String);
 
-fn animate_sprite_system(
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
-) {
-    for (timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-        if timer.finished {
-            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-        }
+fn setup(mut commands: Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(_person: &Person, name: &Name) {
+    println!("hello {}!", name.0);
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(setup.system())
+            .add_system(greet_people.system());
     }
 }
 
-fn setup(
-    commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let texture_handle = asset_server.load("mage/idle-run.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 6, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    commands
-        .spawn(Camera2dBundle::default())
-        .spawn(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(6.0)),
-            ..Default::default()
-        })
-        .with(Timer::from_seconds(0.1, true));
-}
