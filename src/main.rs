255fbe8c3cca8028ef1eaabf520a8373b09dab0f
Mario Martins <mariopmartins@gmail.com>
2020-11-23 13:52:03 -0800

    spawning vehicles

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use std::fmt;
 
 fn main() {
     App::build()
@@ -7,18 +8,53 @@ fn main() {
         .run();
 }
 
-struct Person;
-struct Name(String);
+impl fmt::Display for VehicleStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           VehicleStatus::Parked => write!(f, "Parked"),
+           VehicleStatus::Driving => write!(f, "Driving"),
+       }
+    }
+}
+
+enum VehicleStatus {
+    Parked,
+    Driving
+}
+
+impl fmt::Display for VehicleType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           VehicleType::Car => write!(f, "Car"),
+           VehicleType::Truck => write!(f, "Truck"),
+           VehicleType::Bike => write!(f, "Bike"),
+       }
+    }
+}
+
+enum VehicleType {
+    Car,
+    Truck,
+    Bike
+}
+
+struct Vehicle;
+struct Type(VehicleType);
+struct Status(VehicleStatus);
+
 
 fn setup(mut commands: Commands) {
     commands
-        .spawn((Person, Name("Elaina Proctor".to_string())))
-        .spawn((Person, Name("Renzo Hume".to_string())))
-        .spawn((Person, Name("Zayna Nieves".to_string())));
+        .spawn((Vehicle, Type(VehicleType::Car), Status(VehicleStatus::Parked)))
+        .spawn((Vehicle, Type(VehicleType::Car), Status(VehicleStatus::Driving)))
+        .spawn((Vehicle, Type(VehicleType::Truck), Status(VehicleStatus::Parked)))
+        .spawn((Vehicle, Type(VehicleType::Truck), Status(VehicleStatus::Driving)))
+        .spawn((Vehicle, Type(VehicleType::Bike), Status(VehicleStatus::Parked)))
+        .spawn((Vehicle, Type(VehicleType::Bike), Status(VehicleStatus::Driving)));
 }
 
-fn greet_people(_person: &Person, name: &Name) {
-    println!("hello {}!", name.0);
+fn greet_people(_vehicle: &Vehicle, _type: &Type, _status: &Status) {
+    println!("--> {} {}", _type.0, _status.0);
 }
 
 pub struct HelloPlugin;
